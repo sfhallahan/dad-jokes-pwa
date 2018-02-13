895289ab62c219395b7b4e16f4d8511518d1273a
Hallahan <sean.hallahan@accenturefederal.com>
2018-02-13 09:58:23 -0500

    sw updates

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import {cardContainer, jokeText} from './styles.css'
+import {cardContainer, jokeText, errorText} from './styles.css'
 
 Card.propTypes = {
   jokeText: PropTypes.string.isRequired,
   favorited: PropTypes.bool,
   dateReceived: PropTypes.number,
+  error: PropTypes.string,
 }
 
 export default function Card(props) {
   return (
     <div className={cardContainer}>
-      <p className={jokeText}>
-          {`"${props.jokeText}"`}
-      </p>
+      {props.jokeText
+      ? <p className={jokeText}>{`"${props.jokeText}"`}</p>
+      : <p className={errorText}>{props.error}</p>}
     </div>
   )
 }
