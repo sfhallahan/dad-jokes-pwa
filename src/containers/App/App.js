d2d50f25fb1783bbb1a2b6a5325a31cc6f1f865e
Sean Hallahan <sfhallahan@gmail.com>
2018-04-16 21:25:07 -0400

    netlify setup

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,13 @@
-import React, { Fragment } from 'react'
-import { Route } from 'react-router-dom'
-import { BottomNavContainer, HomeContainer, SearchContainer } from 'containers'
+import React, { Fragment } from "react";
+import { Route } from "react-router-dom";
+import { BottomNavContainer, HomeContainer, SearchContainer } from "containers";
 
-export default function App (props) {
+export default function App(props) {
   return (
     <Fragment>
-      <Route exact path='/' component={HomeContainer} />
-      <Route path='/search' component={SearchContainer} />
+      <Route exact path="/" component={HomeContainer} />
+      <Route path="/search" component={SearchContainer} />
       <BottomNavContainer />
     </Fragment>
-
-  )
+  );
 }
