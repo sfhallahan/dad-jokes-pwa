49d267a5d2061131582382e9a54d45ec2cb2142d
Hallahan <sean.hallahan@accenturefederal.com>
2018-04-16 21:06:40 -0400

    test coverage for Search and Card components

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import {cardContainer, jokeText, errorText} from './styles.css'
+import { cardContainer, jokeText, errorText } from "./styles.css";
 
 Card.propTypes = {
-  jokeText: PropTypes.string.isRequired,
-  favorited: PropTypes.bool,
-  dateReceived: PropTypes.number,
-  error: PropTypes.string,
-}
+	jokeText: PropTypes.string.isRequired,
+	favorited: PropTypes.bool,
+	dateReceived: PropTypes.number,
+	error: PropTypes.string
+};
 
 export default function Card(props) {
-  return (
-    <div className={cardContainer}>
-      {props.jokeText
-      ? <p className={jokeText}>{`"${props.jokeText}"`}</p>
-      : <p className={errorText}>{props.error}</p>}
-    </div>
-  )
+	return (
+		<div className={cardContainer}>
+			{props.jokeText ? (
+				<p className={jokeText} id="jokeText">{`"${props.jokeText}"`}</p>
+			) : (
+				<p className={errorText} id="error">
+					{props.error}
+				</p>
+			)}
+		</div>
+	);
 }
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,43 +1,51 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { CircularProgress } from 'material-ui/Progress';
-import SearchIcon from 'material-ui-icons/Search'
-import { Card } from 'components'
-import { gridContainer, siteTitle } from 'styles/sharedStyles.css'
-import { searchBar, searchInput, resultsContainer, errorText } from './styles.css'
+import React from "react";
+import PropTypes from "prop-types";
+import { CircularProgress } from "material-ui/Progress";
+import SearchIcon from "material-ui-icons/Search";
+import { Card } from "components";
+import { gridContainer, siteTitle } from "styles/sharedStyles.css";
+import { searchBar, searchInput, resultsContainer, errorText } from "./styles.css";
 
 Search.propTypes = {
-  isFetching: PropTypes.bool.isRequired,
-  onSearch: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
-  searchInputText: PropTypes.string.isRequired,
-  jokes: PropTypes.array.isRequired
-}
+	isFetching: PropTypes.bool.isRequired,
+	onSearch: PropTypes.func.isRequired,
+	onChange: PropTypes.func.isRequired,
+	searchInputText: PropTypes.string.isRequired,
+	jokes: PropTypes.array.isRequired
+};
 
 export default function Search(props) {
-  return (
-    <div className={gridContainer}>
-      <h1 className={siteTitle}>DadJokes</h1>
-      <form className={searchBar}
-          onSubmit={(e) => props.onSearch(e)}
-          onClick={() => document.getElementById("searchInput").focus()}>
-        <input
-          id="searchInput"
-          className={searchInput}
-          type='text'
-          value={props.searchInputText}
-          onChange={(e) => props.onChange(e)} />
-        <SearchIcon />
-
-      </form>
-      <div className={resultsContainer}>
-        {props.jokes.length === 0 && props.isFetching === false
-          ? <p className={errorText}>{'No results to display'}</p>
-          : props.jokes.map((joke) => <Card key={joke.id} jokeText={joke.joke} />)
-        }
-        {props.isFetching === true ? <CircularProgress className={errorText} color="accent" size={50} />: null}
-
-      </div>
-    </div>
-  )
+	return (
+		<div className={gridContainer}>
+			<h1 className={siteTitle} id="siteTitle">
+				DadJokes
+			</h1>
+			<form
+				className={searchBar}
+				onSubmit={e => props.onSearch(e)}
+				onClick={() => document.getElementById("searchInput").focus()}
+			>
+				<input
+					id="searchInput"
+					className={searchInput}
+					type="text"
+					value={props.searchInputText}
+					onChange={e => props.onChange(e)}
+				/>
+				<SearchIcon />
+			</form>
+			<div className={resultsContainer}>
+				{props.jokes.length === 0 && props.isFetching === false ? (
+					<p className={errorText} id="error">
+						{"No results to display"}
+					</p>
+				) : (
+					props.jokes.map(joke => <Card key={joke.id} jokeText={joke.joke} />)
+				)}
+				{props.isFetching === true ? (
+					<CircularProgress className={errorText} color="accent" size={50} />
+				) : null}
+			</div>
+		</div>
+	);
 }
