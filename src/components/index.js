16c5b4f299b899b5c8bb4d2f01602157e20f5ef3
Sean Hallahan <sfhallahan@gmail.com>
2018-04-22 20:02:35 -0400

    cleanup

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 // Export components here
 
-export { default as Home } from './Home/Home'
-export { default as Card } from './Card/Card'
-export { default as Search } from './Search/Search'
+export { default as Home } from "./Home/Home";
+export { default as Card } from "./Card/Card";
+export { default as Search } from "./Search/Search";
+export { default as DesktopModal } from "./DesktopModal/DesktopModal";
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,34 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import { Route } from "react-router-dom";
 import { BottomNavContainer, HomeContainer, SearchContainer } from "containers";
+import { DesktopModal } from "components";
 
-export default function App(props) {
-  return (
-    <Fragment>
-      <Route exact path="/" component={HomeContainer} />
-      <Route path="/search" component={SearchContainer} />
-      <BottomNavContainer />
-    </Fragment>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  componentDidMount() {
+    if (window.innerWidth > 900) {
+      this.setState({ isOpen: true });
+    }
+  }
+
+  handleClose = () => {
+    this.setState({ isOpen: false });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Route exact path="/" component={HomeContainer} />
+        <Route path="/search" component={SearchContainer} />
+        <BottomNavContainer />
+        <DesktopModal handleClose={this.handleClose} isOpen={this.state.isOpen} />
+      </Fragment>
+    );
+  }
 }
